99724152a32b16e5cde0a3b75f11faa0d812e892
Michael Witrant <mike@lepton.fr>
2013-03-31 16:53:31 +0200

    dump xml

--- extract_amendments.rb
+++ extract_amendments.rb
@@ -3,7 +3,11 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'zippy'
+require 'clik'
 
+xml_dump_path = nil
+
+cli '--xml-dump' => lambda { |path| xml_dump_path = path }
 
 opendocument_path = ARGV.first
 raise "usage: #$0 <OpenDocument file>" unless opendocument_path
@@ -11,6 +15,10 @@ raise "usage: #$0 <OpenDocument file>" unless opendocument_path
 xml = nil
 Zippy.open(opendocument_path) do |zip|
   xml = zip['content.xml']
+end
+
+if xml_dump_path
+  File.open(xml_dump_path, "w") { |f| f.write xml }
 end
 
 puts xml.size
